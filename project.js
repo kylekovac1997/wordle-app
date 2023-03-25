0be55bed73fdf513ffaf14f15718c0dcd5a2ca92
kylekovac <kylekovac97@outlook.com>
2023-03-25 13:11:09 +1100

    fixing first code

--- project.js
+++ project.js
@@ -1,14 +1,26 @@
 import {validWords} from './wordle-Words.js'
 
-let randomWord = validWords[Math.floor(Math.random() * validWords.length)]
+//Generates random word from wordle-words.js array.
+let randomWord = validWords[Math.floor(Math.random() * validWords.length)];
+let wordlegrid = document.getElementById('wordle_grid'); 
 
-for(let i = 0; i < 5; i++){
-    let wordlegrid = document.getElementById('wordle_grid'); 
-    let divWordDisplay = document.createElement('div');
-    divWordDisplay.setAttribute("class", 'div_word_Display')
-    wordlegrid.appendChild(divWordDisplay);
+//A for loop to create 5 Divs with a class set to div word display.
+for(let i = 1; i <= 5; i++){
     
+    let divWordDisplay = document.createElement('div');
+    divWordDisplay.setAttribute("class", 'div_word_Display');
+    wordlegrid.appendChild(divWordDisplay);
+   
+    //This is assiging the value characters to the random word to split each letter individually. 
     let characters = randomWord.split('');
-    let character = characters[i];
-    divWordDisplay.textContent = character;
+
+    // A for loop here is to assign each individually letter to a P element  and append it diretly to div Word Display.
+    for(let j = 0; j < characters.length; j++){
+   
+        let pElementWordDisplay = document.createElement("P");
+        divWordDisplay.appendChild(pElementWordDisplay);
+        pElementWordDisplay.textContent = characters[j];
+        pElementWordDisplay.classList.add('pElementWordDisplayStyle')
+    }
+    
 }
