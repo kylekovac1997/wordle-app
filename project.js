d1b9f8f9293b8016be165dd401d3914084da7d85
kylekovac <kylekovac97@outlook.com>
2023-03-26 19:25:37 +1100

    fixed checking for correct word error

--- project.js
+++ project.js
@@ -4,12 +4,8 @@ import {validWords} from './wordle-Words.js'
 let randomWord = validWords[Math.floor(Math.random() * validWords.length)];
 let wordlegrid = document.getElementById('wordle_grid'); 
 
-
-
-
 //A for loop to create 5 Divs with a class set to div word display.
 for(let i = 1; i <= 6; i++){
-    let hiddenWord = [];
     
     let divWordDisplay = document.createElement('div');
     divWordDisplay.setAttribute("class", 'div_word_Display');
@@ -17,34 +13,46 @@ for(let i = 1; i <= 6; i++){
    
     //This is assiging the value characters to the random word to split each letter individually. 
     let characters = randomWord.split('');
-
-    hiddenWord.push(characters)
-    console.log(hiddenWord)
     // A for loop here is to assign each individually letter to a P element  and append it diretly to div Word Display.
     for(let j = 0; j < characters.length; j++){
         
-        let pElementWordDisplay = document.createElement("input");
-        divWordDisplay.appendChild(pElementWordDisplay);
-        pElementWordDisplay.textContent = characters[j];
-        pElementWordDisplay.classList.add('pElementWordDisplayStyle')
+      let pElementWordDisplay = document.createElement("input");
+      divWordDisplay.appendChild(pElementWordDisplay);
+      pElementWordDisplay.textContent = characters[j];
+      pElementWordDisplay.classList.add('pElementWordDisplayStyle')
+      pElementWordDisplay.setAttribute('maxlength', 1)
+        
          
-        pElementWordDisplay.addEventListener("input", function(event){
-
-           let inputIndex = [...divWordDisplay.children].indexOf(event.target);
-           let inputValue = event.target.value.toLowerCase();
-           let character = hiddenWord[inputIndex].toLowerCase();
-
-            if (pElementWordDisplay === characters) {
-                event.target.classList.add('correct');
-              } else {
-                event.target.classList.remove('correct');
-              }
-              if (hiddenWord.toLowerCase().includes(inputValue)) {
-                divWordDisplay.classList.add('contains_word');
-              } else {
-                divWordDisplay.classList.remove('contains_word');
-              }
-        })
+      pElementWordDisplay.addEventListener('input', (event) => {
+        let inputElement = event.target;
+        let letterValue = inputElement.value.toUpperCase();
+        let index = Array.from(inputElement.parentElement.children).indexOf(inputElement);
+        let successCount = 0;
+        let successLetters = "";
+              
+        //Checks for both words
+        for (let j in characters) {
+          //if same then green
+          if (letterValue == characters[index]) {
+            inputElement.classList.add("correct");
+            successCount += 1;
+            successLetters += randomWord[j];
+          } else if (
+            characters.includes(letterValue) &&
+            !successLetters.includes(letterValue)
+          ) {
+            //If the letter exists in the chosen word then yellow
+            inputElement.classList.add("partially-correct");
+          } else {
+            //If the letter is wrong, color it red
+            inputElement.classList.add("incorrect");
+          }
+        }
+      });
+      
+      
+      
+        
     }
-}
+  }
 
