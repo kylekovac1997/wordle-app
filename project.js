74090026f0e0f6bd377398d3c9b8f4c26948f8db
kylekovac <kylekovac97@outlook.com>
2023-03-27 19:33:17 +1100

    fixed css style to cneter add more methods to js

--- project.js
+++ project.js
@@ -1,58 +1,81 @@
 import {validWords} from './wordle-Words.js'
 
-//Generates random word from wordle-words.js array.
+//generates the random word from the import of validWords array
 let randomWord = validWords[Math.floor(Math.random() * validWords.length)];
 let wordlegrid = document.getElementById('wordle_grid'); 
+let winCounter = 0
 
-//A for loop to create 5 Divs with a class set to div word display.
+//creates the display grid for the letters
 for(let i = 1; i <= 6; i++){
-    
-    let divWordDisplay = document.createElement('div');
-    divWordDisplay.setAttribute("class", 'div_word_Display');
-    wordlegrid.appendChild(divWordDisplay);
-   
-    //This is assiging the value characters to the random word to split each letter individually. 
-    let characters = randomWord.split('');
-    // A for loop here is to assign each individually letter to a P element  and append it diretly to div Word Display.
-    for(let j = 0; j < characters.length; j++){
-        
-      let pElementWordDisplay = document.createElement("input");
-      divWordDisplay.appendChild(pElementWordDisplay);
-      pElementWordDisplay.textContent = characters[j];
-      pElementWordDisplay.classList.add('pElementWordDisplayStyle')
-      pElementWordDisplay.setAttribute('maxlength', 1)
-        
-         
-      pElementWordDisplay.addEventListener('input', (event) => {
-        let inputElement = event.target;
-        let letterValue = inputElement.value.toUpperCase();
-        let index = Array.from(inputElement.parentElement.children).indexOf(inputElement);
-        let successCount = 0;
-        let successLetters = "";
-              
-        //Checks for both words
-        for (let j in characters) {
-          //if same then green
-          if (letterValue == characters[index]) {
-            inputElement.classList.add("correct");
-            successCount += 1;
-            successLetters += randomWord[j];
-          } else if (
-            characters.includes(letterValue) &&
-            !successLetters.includes(letterValue)
-          ) {
-            //If the letter exists in the chosen word then yellow
-            inputElement.classList.add("partially-correct");
-          } else {
-            //If the letter is wrong, color it red
-            inputElement.classList.add("incorrect");
-          }
-        }
-      });
-      
-      
-      
-        
-    }
-  }
+  let divWordDisplay = document.createElement('div');
+  divWordDisplay.setAttribute("class", 'div_word_Display');
+  wordlegrid.appendChild(divWordDisplay);
+ 
+  let characters = randomWord.split('');
+  for(let j = 0; j < characters.length; j++){  
+    let pElementWordDisplay = document.createElement("input");
+    divWordDisplay.appendChild(pElementWordDisplay);
+    pElementWordDisplay.textContent = characters[j];
+    pElementWordDisplay.classList.add('pElementWordDisplayStyle');
+    pElementWordDisplay.setAttribute('maxlength', 1);
 
+    pElementWordDisplay.addEventListener('input', (event) => {
+      let inputElement = event.target;
+      let letterValue = inputElement.value;
+
+      let index = Array.from(inputElement.parentElement.children).indexOf(inputElement);
+    
+      //function to check if the letter is correct, partially correct or incorrect      
+      for (let j in characters) {
+        if (letterValue.toUpperCase() == characters[index]) {
+          inputElement.classList.add("correct");
+        } else if (
+          characters.includes(letterValue.toUpperCase())) 
+        {
+          inputElement.classList.add("partially-correct");
+        } else {
+          inputElement.classList.add("incorrect");
+        }
+      }
+      
+      // Check if all input elements in the div have values
+      let allInputsFilled = Array.from(inputElement.parentElement.children).every(child => child.value);
+     
+      if (allInputsFilled) {
+        // Disable all input elements in the div
+        Array.from(inputElement.parentElement.children).forEach(child => child.disabled = true);
+        // Call checkWord function
+        checkWord(inputElement.parentElement);
+      } else {
+        // Move focus to the next input element
+        inputElement.nextElementSibling.focus();
+      }
+    });
+  }
+}
+
+//checks players input to make sure its a word
+function checkWord(parentElement) {
+  // Get all the input elements
+  const inputElements = parentElement.querySelectorAll('input');
+
+  // Get an array of input values
+  const inputValues = Array.from(inputElements).map(inputElement => inputElement.value).join('');
+
+  // Check if the input word is valid
+  const isValidWord = validWords.includes(inputValues.toUpperCase());
+
+  if (isValidWord) {
+    // The input word is valid
+    console.log('Valid word');
+    
+    // Move focus to the first input element of the next div
+    const nextDiv = parentElement.nextElementSibling;
+    if (nextDiv) {
+      nextDiv.querySelector('input').focus();
+    }
+  } else {
+    // The input word is invalid
+    console.log('Invalid word');
+  }
+}
