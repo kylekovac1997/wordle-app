feb04b84cd52506d07ee4da9342f516888a303fe
kylekovac <kylekovac97@outlook.com>
2023-03-29 15:05:23 +1100

    v2 new code layout with working keyboard

--- project.js
+++ project.js
@@ -1,88 +1,137 @@
 import { validWords } from './wordle-Words.js';
 
-function initializeWordleGrid() {
-  // Generate the random word from the import of validWords array
-  let randomWord = validWords[Math.floor(Math.random() * validWords.length)];
-  let wordlegrid = document.getElementById('wordle_grid');
-  console.log(randomWord);
+function gameFunction() {
+  const attempts = 6;
+  let currentRow = 0;
+  let currentColumn = 0;
+  let currentWord = '';
 
-  // Creates the display grid for the letters
-  for (let i = 1; i <= 6; i++) {
-    let divWordDisplay = document.createElement('div');
-    divWordDisplay.setAttribute('class', 'div_word_Display');
-    wordlegrid.appendChild(divWordDisplay);
+  const randomWord = () => {
+    currentWord = validWords[Math.floor(Math.random() * validWords.length)];
+  }
 
-    let characters = randomWord.split('');
+  const initializeWordleGrid = () => {
+    let wordleGrid = document.getElementById('wordle_grid');
+    wordleGrid.innerHTML = '';
 
-    for (let j = 0; j < characters.length; j++) {
-      let inputElementWordDisplay = document.createElement('input');
-      divWordDisplay.appendChild(inputElementWordDisplay);
-      inputElementWordDisplay.value = ''; // Set the initial value to empty
-      inputElementWordDisplay.classList.add('pElementWordDisplayStyle');
-      inputElementWordDisplay.setAttribute('maxlength', 1);
+    randomWord();
 
-      inputElementWordDisplay.addEventListener('input', (event) => {
-        let inputElement = event.target;
+    let gridColumns = []; // create an array to hold all grid columns
 
-        // Check if all the input elements in the current div are filled
-        let isDivFilled = true;
-        const inputElementsInDiv = divWordDisplay.querySelectorAll('input');
-        inputElementsInDiv.forEach((input) => {
-          if (!input.value) {
-            isDivFilled = false;
-          }
-        });
+    for (let i = 0; i < attempts; i++) {
+      let gridRow = document.createElement('div');
+      gridRow.className = 'grid-row';
+      gridRow.id = 'row-' + i;
+      wordleGrid.appendChild(gridRow);
 
-        if (isDivFilled) {
-          checkWord(divWordDisplay, inputElementsInDiv, characters); // Check if the word is valid
-        } else {
-          // Move focus to the next input element
-          inputElement.nextElementSibling.focus();
+      for (let j = 0; j < 5; j++) {
+        let gridColumn = document.createElement('div');
+        gridColumn.className = 'grid-column';
+        gridColumn.id = 'column-' + j;
+        gridColumn.value = currentWord[j];
+        console.log(gridColumn.value)
+        gridRow.appendChild(gridColumn);
+        gridColumns.push(gridColumn); // add the column to the array
+
+        if (j === 0) {
+          currentColumn = gridColumn; // set the current column to the first column in the row
+        }
+      }
+    }
+
+    return gridColumns; // return the array of grid columns
+  }
+
+  const keyboard = (gridColumns) => {
+    let keys = document.getElementsByClassName('key');
+    let currentColumn = gridColumns[0];
+  
+    for (let keyElement of keys) {
+      let key = keyElement.textContent;
+      keyElement.addEventListener('click', function() {
+  
+        switch (key) {
+          case '⇦':
+            currentColumn.textContent = currentColumn.textContent.slice(0, -1);
+            if (currentColumn.textContent.length === 0 && currentColumn.previousElementSibling) {
+              currentColumn = currentColumn.previousElementSibling;
+            }
+            break;
+          case 'ent':
+            checkWord(currentWord);
+            currentRow += 1;
+            if (currentRow >= attempts) {
+              currentRow = 0;
+            }
+            currentColumn = gridColumns[currentRow * 5];
+            break;
+          default:
+            if (currentColumn.textContent.length < 1) {
+              currentColumn.textContent += key;
+              if (currentColumn.nextElementSibling) {
+                currentColumn = currentColumn.nextElementSibling;
+              }
+            }
         }
       });
     }
   }
-}
 
-function checkWord(divWordDisplay, inputElements, characters) {
-  let inputValues = '';
+  let gridColumns = initializeWordleGrid(); // get the array of grid columns
 
-  inputElements.forEach((inputElement) => {
-    inputValues += inputElement.value.toUpperCase(); // Combine all the input values
-  });
+  keyboard(gridColumns); // pass the array of grid columns to the keyboard function
 
-  let isValidWord = validWords.includes(inputValues);
-
-  if (isValidWord) {
-    // Move focus to the first input element of the next div
-    const nextDiv = divWordDisplay.nextElementSibling;
-   
-    if (nextDiv) {
-      nextDiv.querySelector('input').focus();
-      
-      for (let char in inputElements) {
-        if (inputValues[char] == characters[char]) {
-          inputElements[char].classList.add('correct');
-        } else if (characters.includes(inputValues[char])) {
-          inputElements[char].classList.add('partially-correct');
-        } else {
-          inputElements[char].classList.add('incorrect');
-        }
+  const checkWord = () => {
+    const wordColumns = document.querySelectorAll('.grid-row#row-' + currentRow + ' .grid-column');
+    const correctColumns = [];
+    const partiallyCorrectColumns = [];
+    const incorrectColumns = [];
+  
+    for (let i = 0; i < currentWord.length; i++) {
+      const userInput = wordColumns[i].textContent;
+      const currentWordLetter = currentWord[i];
+  
+      if (userInput === currentWordLetter) {
+        correctColumns.push(wordColumns[i]);
+      } else if (currentWord.includes(userInput)) {
+        partiallyCorrectColumns.push(wordColumns[i]);
+      } else {
+        incorrectColumns.push(wordColumns[i]);
       }
     }
-  } else {
-    // The input word is invalid
-    alert('Invalid word. Enter a new word');
-    inputElements.forEach((inputElement) => {
-      inputElement.value = ''; // Reset the input values
-      inputElement.disabled = false; // Enable the input elements
-      inputElement.classList.remove('correct', 'partially-correct', 'incorrect');
-    });
-    inputElements[0].focus();
+  
+    if (correctColumns.length === 5) {
+      for (let column of wordColumns) {
+        column.classList.add('correct');
+      }
+      alert('You won!');
+    } else if (correctColumns.length > 0) {
+      for (let column of correctColumns) {
+        column.classList.add('correct');
+      }
+      for (let column of partiallyCorrectColumns) {
+        column.classList.add('partially-correct');
+      }
+      for (let column of incorrectColumns) {
+        column.classList.add('incorrect');
+      }
+    } else {
+      for (let column of incorrectColumns) {
+        column.classList.add('incorrect');
+      }
+    }
+
+    if(wordColumns !== randomWord){
+      alert("invalid word")
+      wordColumns.forEach((column)=>{
+        column.textContent = '';
+        column.disable = false;
+        column.classList.remove('correct', 'partially-correct', 'incorrect');
+        
+      });
+      column[0].focus();
+    }
   }
+}  
 
-
-
-}
-
-initializeWordleGrid();
+gameFunction();
